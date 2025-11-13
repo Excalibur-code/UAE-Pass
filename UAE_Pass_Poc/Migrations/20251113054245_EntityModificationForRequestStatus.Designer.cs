fa7286090b2e74d2aaec5c69bb41baf6a9550170
Harsh Pal <harsh.admin@wasl.ae>
2025-11-13 09:44:52 +0400

    Entity Changes for Reject Notification

--- UAE_Pass_Poc/Migrations/20251113054245_EntityModificationForRequestStatus.Designer.cs
+++ UAE_Pass_Poc/Migrations/20251113054245_EntityModificationForRequestStatus.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UAE_Pass_Poc.DBContext;
 
@@ -11,9 +12,11 @@ using UAE_Pass_Poc.DBContext;
 namespace UAE_Pass_Poc.Migrations
 {
     [DbContext(typeof(UaePassDbContext))]
-    partial class UaePassDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251113054245_EntityModificationForRequestStatus")]
+    partial class EntityModificationForRequestStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UAE_Pass_Poc/Migrations/20251113054245_EntityModificationForRequestStatus.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UAE_Pass_Poc.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntityModificationForRequestStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "RequestPresentation",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "RequestPresentation",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "RejectNotification",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProofOfPresentationRequestId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RejectReason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PresentationRejectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RequestPresentationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RejectNotification", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RejectNotification");
+
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "RequestPresentation");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "RequestPresentation");
+        }
+    }
+}
